caec0cfff8d59507b897146e2c56b5f7b123e385
rbibler <rbibler01@hotmail.com>
2015-12-31 17:42:45 -0500

    Initial Commit

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] musicOrderChangeArray;
+	
+	private AudioSource audioSource;
+
+	void Awake() {
+		DontDestroyOnLoad(gameObject);
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	void OnLevelWasLoaded(int level) {
+		AudioClip thisLevelsMusic = musicOrderChangeArray[level];
+		if(level == 2 || thisLevelsMusic == audioSource.clip) {
+			return;
+		}
+		if(thisLevelsMusic) {
+			audioSource.clip = thisLevelsMusic;
+			audioSource.loop = true;
+			audioSource.Play ();
+		}
+	}
+}
